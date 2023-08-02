c61da74c100e8b238627c5bfe70a1297d37da773
natgitinit <natgitinit@gmail.com>
2023-08-02 17:17:08 -0500

    update node_modules

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,46 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors({ origin: true }));
+
+// app.post("/authenticate", async (req, res) => {
+//   try {
+//     const r = await axios.put(
+//       "https://api.chatengine.io/users/",
+//       { username: username, secret: username, first_name: username },
+//       { headers: { "Private-Key": "ba763a65-ae58-437f-9edc-8d66f0601c67" } }
+//     );
+//     return res.status(r.status).json(r.data);
+//   } catch (e) {
+//     return res.status(e.response.status).json(e.response.data);
+//   }
+// });
+
+// app.listen(3001);
+
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-  // const { username } = req.body;
-  // // Get or create user on Chat Engine!
-  // return res.json({username: username, secret: secret});
+const CHAT_ENGINE_PROJECT_ID = "";
+const CHAT_ENGINE_PRIVATE_KEY = "";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
   try {
-    const r = await axios.put(
+    const r = await axios.post(
       "https://api.chatengine.io/users/",
-      { username: username, secret: username, first_name: username },
-      { headers: { "Private-Key": "ba763a65-ae58-437f-9edc-8d66f0601c67" } }
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
@@ -21,4 +48,24 @@ app.post("/authenticate", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
 app.listen(3001);
