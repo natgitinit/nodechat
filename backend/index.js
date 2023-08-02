1748e836ae11de19721e130aa607ea5e37e150b0
natgitinit <natgitinit@gmail.com>
2023-08-02 16:26:02 -0500

    update endpoint url

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,19 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  // Get or create user on Chat Engine!
-  return res.json({username: username, secret: secret});
-  // try {
-  //   const r = await axios.put(
-  //     "https://api.chatengine.io/users/",
-  //     { username: username, secret: username, first_name: username },
-  //     { headers: { "Private-Key": "ba763a65-ae58-437f-9edc-8d66f0601c67" } }
-  //   );
-  //   return res.status(r.status).json(r.data);
-  // } catch (e) {
-  //   return res.status(e.response.status).json(e.response.data);
-  // }
+  // const { username } = req.body;
+  // // Get or create user on Chat Engine!
+  // return res.json({username: username, secret: secret});
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "ba763a65-ae58-437f-9edc-8d66f0601c67" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
